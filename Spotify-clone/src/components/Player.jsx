d079221815191d216f781c5ba773d3c8cc76b6df
Rasul <r.azimov2005@gmail.com>
2026-02-20 22:34:51 +0200

    Исправить 2

--- Spotify-clone/src/components/Player.jsx
+++ Spotify-clone/src/components/Player.jsx
@@ -44,10 +44,6 @@ const Player = () => {
                 </div>
             </div>
             <div className='hidden lg:flex items-center gap-2 opacity-75'>
-                <img className='w-4' src={assets.plays_icon} alt="" />
-                <img className='w-4' src={assets.mic_icon} alt="" />
-                <img className='w-4' src={assets.queue_icon} alt="" />
-                <img className='w-4' src={assets.speaker_icon} alt="" />
                 <img className='w-4' src={assets.volume_icon} alt="" />
                 <input
                     type="range"
@@ -61,8 +57,6 @@ const Player = () => {
                     onBlur={applyVolume}
                     className='w-20'
                 />
-                <img className='w-4' src={assets.mini_player_icon} alt="" />
-                <img className='w-4' src={assets.zoom_icon} alt="" />
             </div>
         </div>
     )
